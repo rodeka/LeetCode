b140343cd8cc0c7f445e43ad403680d7f6589e56
rodeka <rodeka.core@ya.ru>
2025-04-15 21:17:25 +0300

    1 is done

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -6,15 +6,27 @@
 
 // @lc code=start
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> searchNum;
-        for(int i=0; i<nums.size(); i++){
-            if(searchNum.find(target-nums[i])!=searchNum.end()){
-                return 
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            vector<pair<int, int>> new_nums(nums.size());
+            for(int i=0; i<nums.size(); i++){
+                new_nums[i] = make_pair(nums[i], i);
             }
+            struct{
+                bool operator()(pair<int, int> a, pair<int, int> b) {return a.first < b.first; }
+            }
+            customLess;
+            sort(new_nums.begin(), new_nums.end(), customLess);
+    
+            int i=0, j=new_nums.size()-1;
+            while(new_nums[i].first+new_nums[j].first!=target){
+                if(new_nums[i].first+new_nums[j].first>target)
+                    j--;
+                else
+                    i++;
+            }
+            return {new_nums[i].second, new_nums[j].second};
         }
-    }
-};
+    };
 // @lc code=end
 
--- ultraheader.hpp
+++ ultraheader.hpp
@@ -5,3 +5,4 @@
 using namespace std;
 
 #include <vector>
+#include <algorithm>
